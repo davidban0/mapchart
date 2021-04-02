4c88cabd2521246abe42876d4da27c619d2da66f
darrenabba <dbcount@yahoo.com>
2021-04-01 17:59:23 -0700

    change

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useRef } from "react"
+import * as d3 from "d3"
+import * as topojson from "topojson"
+import './App.css';
+
+const App = () => {
+
+  const inputEl = useRef(null)
+
+  useEffect(() => {
+    var svg = d3.select(inputEl.current).append("svg");
+    var path = d3.geoPath().projection(d3.geoMercator());
+
+    // d3.json("/world-110m.json", function(error, world) {
+    //   if (error) throw error;
+    //   console.log('dave world => ', world)
+    //   svg.selectAll("path")
+    //     .data(topojson.feature(world,world.objects.countries).features)
+    //     .enter().append("path")
+    //     .attr("d", path);
+    // });
+    // test
+
+    fetch("/world-110m.json", {
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+    })
+      .then(response => {
+        if (response.status !== 200) {
+          console.log(`There was a problem: ${response.status}`)
+          return
+        }
+        response.json().then(world => {
+          console.log('dave ===============> ', world)
+
+        svg.selectAll("path")
+        .data(topojson.feature(world,world.objects.countries).features)
+        .enter().append("path")
+        .attr("d", path);
+          
+        })
+      })
+
+  }, [])
+
+  return (
+    <div ref={inputEl}></div>
+  )
+}
+
+export default App
